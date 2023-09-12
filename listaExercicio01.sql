3175c012266c389aa2967cfbf1a6cdf0e54df436
Maria Luísa Ferraz <102704830+Malufzm@users.noreply.github.com>
2023-09-12 15:27:15 -0300

    exercício 3

--- listaExercicio01.sql
+++ listaExercicio01.sql
@@ -5,3 +5,9 @@ SELECT titulo FROM livros;
 SELECT nome
 FROM autores
 WHERE nascimento < '1900-01-01';
+
+/*3º exercício*/
+SELECT titulo 
+FROM livros
+INNER JOIN autores ON livros.autor_id = autores.id
+WHERE autores.nome = 'J.K. Rowling';
