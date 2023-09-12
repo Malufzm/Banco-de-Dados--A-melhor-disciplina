7f65978586a291fca3c2dbaab232eae6dac4ba3b
Maria Luísa Ferraz <102704830+Malufzm@users.noreply.github.com>
2023-09-12 15:30:37 -0300

    exercício 7

--- listaExercicio01.sql
+++ listaExercicio01.sql
@@ -29,3 +29,7 @@ FROM autores
 LEFT JOIN livros ON autores.id = livros.autor_id
 GROUP BY autores.nome;
 
+/*7º exercício*/
+SELECT curso, COUNT(aluno_id) AS alunos_totais
+FROM matriculas
+GROUP BY curso;
