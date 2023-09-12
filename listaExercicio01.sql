4618dd25954c49fd6166f373ccd9c1c61daae4cc
Maria Luísa Ferraz <102704830+Malufzm@users.noreply.github.com>
2023-09-12 15:29:15 -0300

    exercício 5

--- listaExercicio01.sql
+++ listaExercicio01.sql
@@ -17,3 +17,8 @@ SELECT nome
 FROM alunos
 INNER JOIN matriculas ON alunos.id = matriculas.aluno_id
 WHERE matriculas.curso = 'Engenharia de Software';
+
+/*5º exercício*/
+SELECT produto, SUM(receita) AS total_receita
+FROM vendas
+GROUP BY produto;
