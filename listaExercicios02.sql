be519ece53fa4197af8f61641bfd0380972c8dda
Maria Luísa Ferraz <102704830+Malufzm@users.noreply.github.com>
2023-09-26 01:12:10 -0300

    Exercício 10

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -163,3 +163,15 @@ DELIMITER ;
 --WHERE Categoria.Nome = categoriaNome;: O WHERE filtra os resultados para incluir apenas os livros cuja categoria corresponde ao nome da categoria passado como parâmetro.
 --END //: Isso marca o final do bloco da stored procedure.
 --DELIMITER ;: Restaura o delimitador padrão para ;.
+
+Exercício 10:
+DELIMITER //
+CREATE PROCEDURE sp_LivrosESeusAutores()
+BEGIN
+    SELECT Livro.Titulo, CONCAT(Autor.Nome, ' ', Autor.Sobrenome) AS 'Nome do Autor'
+    FROM Livro
+    INNER JOIN Autor_Livro ON Livro.Livro_ID = Autor_Livro.Livro_ID
+    INNER JOIN Autor ON Autor_Livro.Autor_ID = Autor.Autor_ID;
+END //
+DELIMITER ;
+CALL sp_LivrosESeusAutores();
