e207cf368bd5aa4a3e6e553d9a3c8b053afac5fd
Maria Luísa Ferraz <102704830+Malufzm@users.noreply.github.com>
2023-09-26 01:08:02 -0300

    Exercício 6

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -75,3 +75,20 @@ DELIMITER ;
 CALL sp_LivrosAteAno(2010);
 
 CALL sp_LivrosAteAno(2015);
+
+Exercício 6:
+DELIMITER //
+
+CREATE PROCEDURE sp_TitulosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+END //
+
+DELIMITER ;
+
+CALL sp_TitulosPorCategoria('Romance');
+
+CALL sp_TitulosPorCategoria('Ciência');
