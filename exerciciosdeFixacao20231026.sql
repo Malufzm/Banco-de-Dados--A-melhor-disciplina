470655e01996e6ce6f2fd42752291b16fe3b1b0b
Maria Luísa Ferraz <102704830+Malufzm@users.noreply.github.com>
2023-10-26 22:08:37 -0300

    exercicio 2

--- exerciciosdeFixacao20231026.sql
+++ exerciciosdeFixacao20231026.sql
@@ -1,3 +1,4 @@
+1-
 CREATE TRIGGER cliente_inserido_trigger
 AFTER INSERT ON Clientes
 FOR EACH ROW
@@ -5,3 +6,15 @@ BEGIN
     INSERT INTO Auditoria (mensagem, data_hora)
     VALUES ('Novo cliente inserido em ' || NEW.nome, NOW());
 END;
+
+2-
+DELIMITER //
+CREATE TRIGGER TentativaExclusaoClienteNaAuditoria
+BEFORE DELETE ON Clientes
+FOR EACH ROW
+BEGIN
+    INSERT INTO Auditoria (mensagem)
+    VALUES (CONCAT('Tentativa de exclusão do cliente: ', OLD.nome));
+END;
+//
+DELIMITER ;
